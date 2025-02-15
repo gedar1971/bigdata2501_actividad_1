7aef521bed63b75badcfb92ea973904466bad743
gedar1971 <gedar1971@gmail.com>
2025-02-14 20:30:51 -0500

    feat(Actividad_1): :sparkles: create project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bigdata_actividad_1",
+    version="0.0.1",
+    author="German Arbelaez",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl"
+    ]
+    
+    
+)
