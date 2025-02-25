594cc75cc4017885746512cbae74eb812dfe87e2
gedar1971 <german.arbelaez@est.iudigital.edu.co>
2025-02-24 19:47:43 -0500

    feat: :sparkles: update structure project

--- src/script.py
+++ src/script.py
@@ -3,7 +3,8 @@ import json
 import os
 from dotenv import load_dotenv
 from api.apiBase import fetch_data_from_api
-from database.config import create_connection
+from static.db.config  import create_connection
+from utils.helpers import  create_xlsx_file, audit_data
 
 load_dotenv()
 
@@ -12,15 +13,15 @@ def main():
     api_url = 'https://api.coincap.io/v2/assets'
     
     data = fetch_data_from_api(api_url)
-    
-    create_json_file(data)
+
 
     if isinstance(data, dict):
         data = [data]
 
-    # df_api = pd.DataFrame(data)
     df_api = pd.json_normalize(data, record_path=['data'], meta=['timestamp'])
-    print(df_api)
+
+    audit_data(df_api)
+
     create_xlsx_file(df_api)
  
     if os.getenv('DB_CONNECTION'):
@@ -31,14 +32,14 @@ def main():
             print("Datos insertados en la base de datos exitosamente.")
             conn.close()
 
-def create_json_file(data):
-    with open('request_api_json.json', 'w') as json_file:
-        json.dump(data, json_file, indent=4)
-    print("Archivo JSON 'request_api_json.json' generado exitosamente.")
+# def create_json_file(data):
+#     with open('request_api_json.json', 'w') as json_file:
+#         json.dump(data, json_file, indent=4)
+#     print("Archivo JSON 'request_api_json.json' generado exitosamente.")
 
-def create_xlsx_file(df):
-    df.to_excel('request_api_xlsx.xlsx', index=False)
-    print("Archivo Excel 'request_api_xlsx.xlsx' generado exitosamente.")
+# def create_xlsx_file(df):
+#     df.to_excel('src/static/xlsx/request_api_xlsx.xlsx', index=False)
+#     print("Archivo Excel 'request_api_xlsx.xlsx' generado exitosamente.")
 
 if __name__ == '__main__':
     main()
